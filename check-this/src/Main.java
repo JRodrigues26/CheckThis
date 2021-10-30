caf954afa3609966a7e9a88c0abfa8a3ceda2a95
Baljeet_ <ricardoldias123@gmail.com>
2021-10-30 17:02:57 +0000

    Arranged the server

--- check-this/src/Main.java
+++ check-this/src/Main.java
@@ -1,12 +1,13 @@
+
 public class Main {
 
     private static final int DEFAULT_PORT = 55555;
 
     public static void main(String[] args) {
 
-     Server testserver = new Server(DEFAULT_PORT);
+     Server testServer = new Server(DEFAULT_PORT);
 
-     testserver.openServer();
+     testServer.openServer();
 
     }
 }
