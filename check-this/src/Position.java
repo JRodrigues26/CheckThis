9d3db99e0d42dbc91cd1a2f6373bb0aa91de7530
JRodrigues91 <jdprodrigues@gmail.com>
2021-10-30 17:31:11 +0000

    update on Position

--- check-this/src/Position.java
+++ check-this/src/Position.java
@@ -3,11 +3,24 @@ public class Position {
     private int col;
     private int row;
 
-    public Position() {
-
+    public Position(int x, int y) {
+        col = x;
+        row = y;
     }
 
+    public void setCol(int col) {
+        this.col = col;
+    }
 
+    public void setRow(int row) {
+        this.row = row;
+    }
 
+    public int getCol() {
+        return col;
+    }
 
+    public int getRow() {
+        return row;
+    }
 }
